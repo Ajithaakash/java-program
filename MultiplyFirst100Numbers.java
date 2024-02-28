c62333d22a6e83e15f9555dc823bd3476bbf2742
Ajithaakash <161406958+Ajithaakash@users.noreply.github.com>
2024-02-28 22:03:41 +0530

    Add files via upload

---
+++ MultiplyFirst100Numbers.java
@@ -0,0 +1,11 @@
+public class MultiplyFirst100Numbers {
+    public static void main(String[] args) {
+        long product = 1;
+
+        for (int i = 1; i <= 100; i++) {
+            product *= i;
+        }
+
+        System.out.println("The product of the first 100 numbers is: " + product);
+    }
+}
